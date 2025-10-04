22e29ce9f40c4d21a8768e95f1c2b3a1b3ec2050
ohnuma <k.a.y.0818.k@gmail.com>
2025-10-04 19:17:46 +0900

    refactor: main.rsをスッキリさせる

--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,70 +1,9 @@
-use std::net::{Ipv4Addr, SocketAddr};
-
-use anyhow::{Context, Result};
-use axum::Router;
-use interface::route::{health::build_health_check_routers, version::build_version_routers};
-use shared::env::{which_env, Environment};
-use tokio::net::TcpListener;
-use tower_http::{
-    request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
-    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
-};
-use tracing::Level;
-use tracing_error::ErrorLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use anyhow::Result;
+use backend::{init_logger, run};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     init_logger()?;
     run().await?;
     Ok(())
-}
-
-fn init_logger() -> Result<()> {
-    let env = which_env()?;
-    let log_level = match env {
-        Environment::Dev => "debug",
-        _ => "info",
-    };
-    let env_filter = tracing_subscriber::EnvFilter::from(log_level);
-
-    let subscriber = tracing_subscriber::fmt::layer()
-        .json()
-        .with_file(true)
-        .with_line_number(true)
-        .with_target(false);
-
-    tracing_subscriber::registry()
-        .with(subscriber)
-        .with(env_filter)
-        .with(ErrorLayer::default())
-        .try_init()?;
-
-    Ok(())
-}
-
-async fn run() -> Result<()> {
-    let app = Router::new()
-        .merge(build_health_check_routers())
-        .merge(build_version_routers())
-        .layer(SetRequestIdLayer::x_request_id(MakeRequestUuid))
-        .layer(PropagateRequestIdLayer::x_request_id())
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
-                .on_request(DefaultOnRequest::new().level(Level::INFO))
-                .on_response(DefaultOnResponse::new().level(Level::INFO)),
-        );
-
-    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await?;
-    tracing::info!("Listening on {}", addr);
-    axum::serve(listener, app)
-        .await
-        .context("Unexpected error happened in server")
-        .inspect_err(|e| {
-            tracing::error!(
-                error.cause_chain = ?e,error.message = %e, "Unexpected error"
-            )
-        })
 }
