6c7c577caa4ec0bf88485e9ab9b602f529779548
ohnuma <k.a.y.0818.k@gmail.com>
2025-10-12 00:31:15 +0900

    feat: handler層にも渡す

---
+++ backend/interface/src/handler/problem.rs
@@ -0,0 +1,19 @@
+use axum::http::StatusCode;
+use registry::Registry;
+use tracing::error;
+use usecase::problem::ImportProblemsUsecase;
+
+
+pub async fn import_problem(reg: &Registry) -> StatusCode {
+    let atcoder_problems_port = reg.atcoder_problems_port();
+    let problems_repository = reg.problem_repository();
+    let usecase =  ImportProblemsUsecase::new(atcoder_problems_port, problems_repository);
+
+    match usecase.execute().await {
+        Ok(_) => StatusCode::OK,
+        Err(e) => {
+            error!(error = ?e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
