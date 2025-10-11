cc51a23c6b72ddb880e8b85ab5ff8c51f5b270c5
ohnuma <k.a.y.0818.k@gmail.com>
2025-10-12 00:17:42 +0900

    feat: problems格納のusecaseを実装

--- backend/registry/src/lib.rs
+++ backend/registry/src/lib.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
 use domain::ports::{
-    external::atcoder_problems::AtcoderProblemsPort, repository::health::HealthCheckRepository,
+    external::atcoder_problems::AtcoderProblemsPort, repository::{health::HealthCheckRepository, problem::ProblemRepository},
 };
 use infrastructure::{
     client::atcoder_problems::build_atcoder_problems_client, database::connect_database_with,
-    ports::repository::health::HealthCheckRepositoryImpl,
+    ports::repository::{health::HealthCheckRepositoryImpl, problem::ProblemRepositoryImpl},
 };
 use shared::config::AppConfig;
 
@@ -13,6 +13,7 @@ use shared::config::AppConfig;
 pub struct Registry {
     atcoder_problems_port: Arc<dyn AtcoderProblemsPort>,
     health_check_repository: Arc<dyn HealthCheckRepository>,
+    problem_repository: Arc<dyn ProblemRepository>
 }
 
 impl Registry {
@@ -21,14 +22,22 @@ impl Registry {
             Arc::new(build_atcoder_problems_client(&config.atcoder_problems));
         let pool = connect_database_with(&config.database);
         let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool.to_owned()));
+        let problem_repository = Arc::new(ProblemRepositoryImpl::new(pool.to_owned()));
 
         Self {
             atcoder_problems_port: atcoder_problems_client,
             health_check_repository,
+            problem_repository
         }
     }
 
     pub fn health_check_repository(&self) -> Arc<dyn HealthCheckRepository> {
         self.health_check_repository.to_owned()
+    }
+    pub fn problem_repository(&self) -> Arc<dyn ProblemRepository> {
+        self.problem_repository.to_owned()
+    }
+    pub fn atcoder_problems_port(&self) -> Arc<dyn AtcoderProblemsPort> {
+        self.atcoder_problems_port.to_owned()
     }
 }
---
+++ backend/usecase/src/problem/mod.rs
@@ -0,0 +1,34 @@
+use std::sync::Arc;
+
+use derive_new::new;
+use domain::{
+    model::problem::Problem,
+    ports::{
+        external::atcoder_problems::AtcoderProblemsPort, repository::problem::ProblemRepository,
+    },
+};
+use itertools::Itertools;
+use shared::error::{external::ExternalError, repository::RepositoryError};
+
+#[derive(thiserror::Error, Debug)]
+pub enum ImportUsecaseError {
+    #[error(transparent)]
+    Fetch(#[from] ExternalError),
+    #[error(transparent)]
+    Repository(#[from] RepositoryError),
+}
+
+#[derive(new)]
+pub struct ImportProblemsUsecase {
+    atcoder_problems_port: Arc<dyn AtcoderProblemsPort>,
+    problem_repository: Arc<dyn ProblemRepository>,
+}
+
+impl ImportProblemsUsecase {
+    pub async fn execute(&self) -> Result<(), ImportUsecaseError> {
+        let api_items = self.atcoder_problems_port.fetch_problems().await?;
+        let problems = api_items.into_iter().map(Problem::from).collect_vec();
+        self.problem_repository.create_records(problems).await?;
+        Ok(())
+    }
+}
