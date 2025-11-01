f623b16b13b953a57d6f50e99bd2f2f931b48c73
ohnuma <k.a.y.0818.k@gmail.com>
2025-11-01 12:35:19 +0900

    feat(backend): create solutionのdomain定義

--- backend/domain/src/ports/external/id.rs
+++ backend/domain/src/ports/external/id.rs
@@ -2,5 +2,4 @@ use uuid::Uuid;
 
 pub trait IdProviderPort: Send + Sync {
     fn new_solution_id(&self) -> Uuid;
-    fn new_tag_id(&self) -> Uuid;
 }
---
+++ backend/domain/src/ports/repository/solution/tx.rs
@@ -0,0 +1,30 @@
+use async_trait::async_trait;
+use shared::error::repository::RepositoryError;
+use uuid::Uuid;
+
+use crate::model::solution::Solution;
+
+#[async_trait]
+pub trait SolutionRespositoryTx: Send + Sync {
+    async fn create(&mut self, s: &Solution) -> Result<Uuid, RepositoryError>;
+    async fn replace_tags(&mut self, solution_id: Uuid, tag_id: &Vec<Uuid>) -> Result<(), RepositoryError>;
+}
+
+#[async_trait]
+pub trait TagRepositoryTx: Send + Sync {
+    async fn upsert(&mut self, names: &Vec<String>) -> Result<Vec<Uuid>, RepositoryError>;
+}
+
+#[async_trait]
+pub trait UnitOfWork: Send + Sync {
+    fn solutions(&mut self) -> &mut dyn SolutionRespositoryTx;
+    fn tags(&mut self) -> &mut dyn TagRepositoryTx;
+
+    async fn commit(self: Box<Self>) -> Result<(), RepositoryError>;
+    async fn rollback(self: Box<Self>) -> Result<(), RepositoryError>;
+}
+
+#[async_trait]
+pub trait SolutionTxManager: Send + Sync {
+    async fn begin(&self) -> Result<Box<dyn UnitOfWork>, RepositoryError>;
+}
