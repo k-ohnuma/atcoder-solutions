8c3081a3338f84e95a881beb3a1692ffb1744326
ohnuma <k.a.y.0818.k@gmail.com>
2025-11-01 12:55:33 +0900

    fix(backend): userに終わってる名前role, colorが入っていたらerrorにする

--- backend/infrastructure/src/model/user.rs
+++ backend/infrastructure/src/model/user.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 
 use chrono::{DateTime, Utc};
 use domain::model::user::{Color, Role, User};
+use shared::error::repository::RepositoryError;
 
 pub struct UserRow {
     pub id: String,
@@ -12,14 +13,19 @@ pub struct UserRow {
     pub updated_at: DateTime<Utc>,
 }
 
-impl From<UserRow> for User {
-    fn from(value: UserRow) -> Self {
-        Self {
+impl TryFrom<UserRow> for User {
+    type Error = RepositoryError;
+    fn try_from(value: UserRow) -> Result<User, Self::Error> {
+        let role = Role::from_str(value.role.as_str())
+            .map_err(|e| RepositoryError::Unexpected(e.to_string()))?;
+        let color = Color::from_str(value.color.as_str())
+            .map_err(|e| RepositoryError::Unexpected(e.to_string()))?;
+
+        Ok(User {
             id: value.id,
-            role: Role::from_str(value.role.as_str()).expect("role must be admin or user"),
-            color: Color::from_str(value.color.as_str())
-                .unwrap_or_else(|_| panic!("color is invalid: {}", value.color.as_str())),
+            role,
+            color,
             user_name: value.user_name,
-        }
+        })
     }
 }
