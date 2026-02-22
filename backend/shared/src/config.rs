d3e07d21102586051dec5b863e6bd94360987be4
ohnuma <k.a.y.0818.k@gmail.com>
2026-02-22 23:21:18 +0900

    update: APP_DATABASE_URLでの接続対応

--- backend/shared/src/config.rs
+++ backend/shared/src/config.rs
@@ -11,12 +11,28 @@ impl AppConfig {
         let atcoder_problems = AtcoderProblemsConfig {
             base_endpoint: std::env::var("ATCODER_PROBLEMS_BASE_ENDPOINT")?,
         };
-        let database = DatabaseConfig {
-            host: std::env::var("DATABASE_HOST")?,
-            port: std::env::var("DATABASE_PORT")?.parse()?,
-            username: std::env::var("DATABASE_USERNAME")?,
-            password: std::env::var("DATABASE_PASSWORD")?,
-            database: std::env::var("DATABASE_NAME")?,
+        let app_database_url = std::env::var("APP_DATABASE_URL").ok();
+        let database = if app_database_url.is_some() {
+            DatabaseConfig {
+                app_database_url,
+                host: std::env::var("DATABASE_HOST").unwrap_or_default(),
+                port: std::env::var("DATABASE_PORT")
+                    .ok()
+                    .and_then(|v| v.parse().ok())
+                    .unwrap_or(5432),
+                username: std::env::var("DATABASE_USERNAME").unwrap_or_default(),
+                password: std::env::var("DATABASE_PASSWORD").unwrap_or_default(),
+                database: std::env::var("DATABASE_NAME").unwrap_or_default(),
+            }
+        } else {
+            DatabaseConfig {
+                app_database_url: None,
+                host: std::env::var("DATABASE_HOST")?,
+                port: std::env::var("DATABASE_PORT")?.parse()?,
+                username: std::env::var("DATABASE_USERNAME")?,
+                password: std::env::var("DATABASE_PASSWORD")?,
+                database: std::env::var("DATABASE_NAME")?,
+            }
         };
         let auth = AuthConfig {
             project_id: std::env::var("FIREBASE_PROJECT_ID")?,
@@ -35,6 +51,7 @@ pub struct AtcoderProblemsConfig {
 }
 
 pub struct DatabaseConfig {
+    pub app_database_url: Option<String>,
     pub host: String,
     pub port: u16,
     pub username: String,
